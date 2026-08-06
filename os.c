0e31e4e0e231da15571637ad3dcebf49060d43d2
dweadon <morkveangg@gmail.com>
2026-08-06 10:44:20 +0300

    you can see working directory now, also cd actually works.

--- os.c
+++ os.c
@@ -5,6 +5,18 @@
 #include <sys/wait.h>   // waitpid — wait for commands to finish
 #include <sys/types.h>  // pid_t and other basic types
 
+
+void cd(char *input){
+    char *path;
+    if (strcmp(input, "cd") == 0) {
+        path = getenv("HOME");
+    } else {
+        path = input + 3;   // everything after first 3 symbols, used in order to not count cd.
+    }
+    if (chdir(path) != 0) {
+        perror("cd");
+    }
+}
 void executes (char *input){
     pid_t pid = fork();
     if (pid == 0){
@@ -18,18 +30,25 @@ void executes (char *input){
 
 int main() {
     char input[1028];
+    char cwd[1028];
     system("clear");
     while(1){
-    printf("A\\>> ");
-    fgets(input, sizeof(input), stdin);
-    executes(input);
+        getcwd(cwd, sizeof(cwd));
+        printf("@main~%s$ ", cwd);
+
     if(fgets(input, sizeof(input), stdin) == NULL){
             break;
     }
     input[strcspn(input, "\n")] = 0;
-        
+
     if (strcmp(input, "exit") == 0){
             break;
         }
+    if (strncmp(input, "cd", 2) == 0){
+        cd(input);
+        continue;
+    }
+    executes(input);
+
 }
 }
